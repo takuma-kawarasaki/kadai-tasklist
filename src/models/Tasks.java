cce4d786f7bd51995fc3e39498e531507952123d
takuma.kawarasaki <kawarasaki@jprs.co.jp>
2020-12-08 16:25:13 +0900

    modified model file

--- src/models/Tasks.java
+++ src/models/Tasks.java
@@ -7,9 +7,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(
+        name = "getAllTasks",
+        query = "SELECT m FROM Tasks AS m ORDER BY m.id DESC"
+    ),
+    @NamedQuery(
+            name = "getTasksCount",
+            query = "SELECT COUNT(m) FROM Tasks AS m"
+    )
+})
 @Table(name = "tasks")
 public class Tasks {
     @Id
